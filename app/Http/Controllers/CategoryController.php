0c463398accb71a75da54b8abc11d150ddf0363c
Tiafeno <tiafenofnel@gmail.com>
2022-10-02 00:16:22 +0300

    Add category archive page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\ProductModel;
 use App\Traits\CategoryManager;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
     use CategoryManager;
+
+		public function index($category_slug) {
+			$category = Category::query()->where('slug_name', '=', $category_slug)->first();
+			if ($category) {
+				$products = DB::table('product_has_category')->where([
+						'id_category' => $category->id_category
+				])->get();
+				return view('pages.archive-category', [
+						'category' => $category,
+					'articles' => $products
+				]);
+			} else {
+				return abort(404);
+			}
+		}
 }
---
+++ resources/views/pages/archive-category.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container mt-lg-5 mt-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div>
+                            <h3 class="mt-0 text-center product-single-title">{{ $category->name }} </h3>
+                        </div>
+                        <div class="row">
+                            @if(!empty($articles))
+                                @foreach($articles as $article)
+                                <div class="col-md-4 col-xs-12">
+                                    <x-product-item :product-id="$article->id_product"></x-product-item>
+                                </div>
+                                @endforeach
+                            @endif
+                            @if(empty($article))
+                                <div class="col-md-4 col-xs-12">
+                                    Aucun
+                                </div>
+                            @endif
+                        </div> <!-- end row-->
+
+                    </div> <!-- end card-body-->
+                </div> <!-- end card-->
+            </div> <!-- end col-->
+        </div>
+    </div>
+@endsection
