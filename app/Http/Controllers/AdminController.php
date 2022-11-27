83c873330f1be41732bc03e99d5fc6e75a68b9d5
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2022-11-27 11:10:03 +0300

    add employee

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Traits\OrderManager;
 use App\Traits\ProductManager;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberUtil;
 
 class AdminController extends Controller
 {
@@ -15,6 +20,37 @@ class AdminController extends Controller
   }
 
   public function employee_index() {
+    $users = User::query()->where('group', "=", "admin")->get();
+
+  }
+
+  public function employee_created(){
+    return;
+  }
+
+  public function employee_store(Request $request) {
+    $validator = Validator::make($request->all(), array(
+      "phone" => "required",
+      "password" => "required",
+      "first_name" => "required",
+      "last_name" => "nullable",
+      "email" => "nullable"
+    ))->validate();
+    try {
+      $phoneUtil = PhoneNumberUtil::getInstance();
+      $swissNumber = $phoneUtil->parse($request->get('phone'), "MG");
+      if ($phoneUtil->isValidNumber($swissNumber)) {
+        $phone = $swissNumber->getNationalNumber();
+      }
+      User::create([
+        'first_name' => $request->get('first_name'),
+        'last_name' => $request->get('last_name'),
+        'phone' => "0{$phone}",
+        'email' => $request->get('email', ''),
+        "group" => "admin",
+        'password' => Hash::make($request->get('password')),
+      ]);
+    } catch (NumberParseException $e) { }
     
   }
 
